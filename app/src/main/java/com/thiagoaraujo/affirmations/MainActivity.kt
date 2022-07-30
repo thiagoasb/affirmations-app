81734469827e2c199ba943fc56e14f4c2bb66cb5
thiagoasb <thiagoasbezerra@gmail.com>
2022-07-30 18:57:06 -0300

    Add affirmation string resource and view of size of list"

--- app/src/main/java/com/thiagoaraujo/affirmations/MainActivity.kt
+++ app/src/main/java/com/thiagoaraujo/affirmations/MainActivity.kt
@@ -2,10 +2,16 @@ package com.thiagoaraujo.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.thiagoaraujo.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = Datasource().loadAffirmations().size.toString()
+
     }
 }
